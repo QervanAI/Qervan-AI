f52ed3c22cd00fba4832173b0e2d4043532bb20a
Nuzon AI <NuzonAI@outlook.com>
2025-05-11 06:48:51 +0800

    Create opcua_adapter.cpp

---
+++ services/edge_fabric/iot_bridge/opcua_adapter.cpp
@@ -0,0 +1,6 @@
+// opcua_adapter.cpp - Industrial IoT Protocol Bridge
+#include <open62541/server_config_default.h>
+#include <open62541/plugin/log_stdout.h>
+#include <open62541/client_config_default.h>
+#include <open62541/client_highlevel.h>
+#include <a>
